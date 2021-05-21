fe2a2d880d8bd067656f2d2001dbe6a90bcd7661
Hayden Pennington <haydenpennington@icloud.com>
2021-05-20 20:23:20 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>ClickMap.io</title>
+        <meta name="ClickMap.io" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1>ClickMap.io</h1>
+      </main>
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  )
+}
