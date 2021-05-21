1b8a61b52e58b5ae555a618dbb23c70b93be4625
Hayden Pennington <haydenpennington@icloud.com>
2021-05-20 20:41:41 -0500

    Remove favicon.ico

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>ClickMap.io</title>
         <meta name="ClickMap.io" />
-        <link rel="icon" href="/favicon.ico" />
+        // <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <h1>ClickMap.io</h1>
